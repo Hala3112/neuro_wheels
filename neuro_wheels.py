28c014b4fe3c5aa8001d9430f6a7562b8e377308
Hala3112 <halazhfarfoura@gmail.com>
2025-05-06 14:04:32 +0300

    Update neuro_wheelsafham.py

--- neuro_wheels.py
+++ neuro_wheels.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1PdC8uc28kz6-bvF2JUWaNg2OL6qqi2ob
 """
 
-
 import streamlit as st
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import torch
